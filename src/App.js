ad6bff8aaabbdf735be1058d54912cf6efb767c5
Pranjal Goyal <pranjalgoyal13@gmail.com>
2022-09-23 21:31:54 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import PasswordStrengthMeter from './components/PasswordStrengthMeter';
+
+function App() {
+  const [ password, setPassword ] = useState('');
+
+  return (
+    <div className="container">
+      <div className="col-md-6 mx-auto text-right">
+        <h3 className="text-center my-5">Password Validator</h3>
+        <div className="form-group mb-1">
+          <input
+            type="password"
+            className="form-control shadow-none"
+            placeholder="Password"
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <PasswordStrengthMeter password={password} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
